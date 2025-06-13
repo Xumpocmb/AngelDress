3da30c7457e9d3373c618df22e0323dc8f8a59be
xumpocmb <youarenear26@gmail.com>
2025-06-13 14:34:24 +0300

    post page & styles

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from app_blog.views import blog_view
+from app_blog.views import blog_view, post_view
 
 app_name = 'app_blog'
 urlpatterns = [
     path('', blog_view, name='blog'),
+    path('post', post_view, name='post'),
 ]
--- app_blog/views.py
+++ app_blog/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def blog_view(request):
     return render(request, 'app_blog/blog.html')
+
+
+def post_view(request):
+    return render(request, 'app_blog/post.html')
