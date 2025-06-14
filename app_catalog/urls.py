7cb7001c7e2ee6fc1f0a8364dfb22d4945f04998
xumpocmb <youarenear26@gmail.com>
2025-06-14 17:24:34 +0300

    product page init

--- app_catalog/urls.py
+++ app_catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from app_catalog.views import catalog_view
+from app_catalog.views import catalog_view, product_view
 
 app_name = 'app_catalog'
 urlpatterns = [
     path('', catalog_view, name='catalog'),
+    path('product/', product_view, name='product'),
 ]
--- app_catalog/views.py
+++ app_catalog/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def catalog_view(request):
     return render(request, 'app_catalog/catalog.html')
+
+
+def product_view(request):
+    return render(request, 'app_catalog/product.html')
